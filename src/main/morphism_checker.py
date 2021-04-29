4ca7cec0804ec90a3f5ae24dbdcfeb6a93b41286
Wojciech <w.wegrzynek@gmail.com>
2021-04-29 18:09:44 +0200

    checker check 2. naive implementation

--- src/main/morphism_checker.py
+++ src/main/morphism_checker.py
@@ -1,6 +1,7 @@
 
 from ..utils.pansiot_coder import PansiotCoder
 from ..utils.morphism import Morphism
+import math
 
 class MorphismChecker():
     def __init__(self, pansiot_coder):
@@ -19,16 +20,25 @@ class MorphismChecker():
         curr_word=word_to_check[:k-1]
         i=k
         while i<len(word_to_check):
-            # print(curr_word)
             if curr_word in substrings_so_far:
                 return False
             substrings_so_far.add(curr_word)
             curr_word=curr_word[1:]+word_to_check[i]
             i+=1
 
-        #TODO: check 2.
+        # 2. Do a naive iteration through all subwords pe of size such that e of size less than k-1 would be sufficient for an exponent bigger than (k+1)/k
+        # k-1>|e|>|p|/(k-1)
+        # |p|<(k-1)(k-1)
+        for p_length in range((k-1)**2):
+            if p_length==0:
+                continue
+            # |e|>|p|/(k-1)
+            e_length=math.ceil((p_length+1)/(k-1))
+            for i in range(len(word_to_check)-p_length-e_length):
+                if word_to_check[i:i+e_length]==word_to_check[i+p_length:i+p_length+e_length]:
+                    print(word_to_check[i:i+p_length+e_length])
+                    return False
 
-        # print(substrings_so_far)
         return True
 
 
